a0af5c187e5c0892c502f9a05771a0e9a2c45050
Bernardo Barros <bernardo.barros@protonmail.ch>
2023-10-20 01:48:05 -0400

    sketch rtm/apl

--- src/Music/Time/Rtm.hs
+++ src/Music/Time/Rtm.hs
@@ -8,12 +8,12 @@ module Music.Time.Rtm where
 
 
 data RtmValue
-  = RtmNote Int -- Represents a musical note, where Int is likely the pitch or length
-  | RtmRest Int -- Represents a musical rest, where Int is likely the length of the rest
-  | RtmLeaf Int RtmProportions -- Represents a leaf node, with Int as an identifier or property and RtmProportions as the sequence under this leaf
+  = RtmNote Int 
+  | RtmRest Int 
+  | RtmLeaf Int RtmProportions 
   deriving (Eq, Ord, Show)
 
-data RtmProportions = RtmProportions [RtmValue] -- Represents a sequence of musical values (notes, rests, leaves)
+data RtmProportions = RtmProportions [RtmValue] 
   deriving (Eq, Ord, Show)
 
 
@@ -24,7 +24,7 @@ data RtmStructure
     | RtmVector Int [RtmStructure]
     deriving (Eq, Ord, Show)
 
-{- | structureOfRtm / structureOfRtm'takes a RtmValue / RtmProportions and returns a RtmStructure
+{- 
 >>> tree1 = RtmLeaf 1 (RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4]), RtmRest 3])
 >>> structureOfRtm tree1 
 RtmVector 3 [RtmScalar,RtmVector 2 [RtmScalar,RtmScalar],RtmScalar]
@@ -49,48 +49,50 @@ structureOfRtm (RtmRest _) = RtmScalar
 structureOfRtm (RtmLeaf _ proportions) = RtmVector (countRtmProportions proportions) (structureOfRtm' proportions)
 
 
+{-
+tree5 = RtmLeaf 1 (RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4]), RtmRest 3])
+shapeOfRtm tree5 
+-- [1,3,1,2]
 
+tree2 = RtmLeaf 1 (RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4, RtmLeaf 3 (RtmProportions [RtmRest 2])]), RtmRest 3])
+shapeOfRtm tree2 
+-- [1,3,1,3,1,1]
 
+tree3 = RtmLeaf 4 (RtmProportions [RtmRest 3])
+shapeOfRtm tree3 
+-- [1,1]
+-}
 shapeOfRtm :: RtmValue -> [Int]
 shapeOfRtm (RtmNote _) = []
 shapeOfRtm (RtmRest _) = []
 shapeOfRtm (RtmLeaf _ proportions) = 1 : shapeOfRtmProportions proportions
 
+
 shapeOfRtmProportions :: RtmProportions -> [Int]
 shapeOfRtmProportions (RtmProportions values) =
     length values : mergeShapes (map shapeOfRtm values)
 
--- Merge multiple shapes into one by taking the longest of each depth
+{- | Merge multiple shapes into one by taking the longest of each depth
+>>> mergeShapes [[1, 2], [1, 2, 3], [1]] == [1,2,3]
+>>> mergeShapes [[1, 2, 3], [1], [1, 2]] == [1,2,3]
+>>> mergeShapes [[1, 2], [1, 2], [1, 2]] == [1,2]
+True
+True
+True
+-}
 mergeShapes :: [[Int]] -> [Int]
-mergeShapes = foldr (zipWithMax) []
+mergeShapes = foldr zipWithMax []
   where
     zipWithMax xs ys = zipWith max xs (ys ++ repeat 0) ++ drop (length xs) ys
 
-{- 
-main :: IO ()
-main = do
-    let tree1 = RtmLeaf 1 (RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4]), RtmRest 3])
-    let tree2 = RtmLeaf 1 (RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4, RtmLeaf 3 (RtmProportions [RtmRest 2])]), RtmRest 3])
-    let tree3 = RtmLeaf 4 (RtmProportions [RtmRest 3])
-
-    print $ shapeOfRtm tree1
-    print $ shapeOfRtm tree2
-    print $ shapeOfRtm tree3
-
-    -- Some examples to show how mergeShapes works
-    print $ mergeShapes [[1, 2], [1, 2, 3], [1]]
-    print $ mergeShapes [[1, 2, 3], [1], [1, 2]]
-    print $ mergeShapes [[1, 2], [1, 2], [1, 2]]
-
- -}
 
 leafRanks :: RtmValue -> [(RtmValue, Int)]
 leafRanks val = leafRanksHelper val 0
 
 leafRanksHelper :: RtmValue -> Int -> [(RtmValue, Int)]
 leafRanksHelper (RtmLeaf _ (RtmProportions rtmVals)) depth = concatMap (\v -> leafRanksHelper v (depth + 1)) rtmVals
-leafRanksHelper (RtmNote n) depth = [((RtmNote n), depth)]
-leafRanksHelper (RtmRest n) depth = [((RtmRest n), depth)]
+leafRanksHelper (RtmNote n) depth = [(RtmNote n, depth)]
+leafRanksHelper (RtmRest n) depth = [(RtmRest n, depth)]
 
 leafRanksFromProportions :: RtmProportions -> [(RtmValue, Int)]
 leafRanksFromProportions (RtmProportions rtmVals) = leafRanksHelperForProportions rtmVals 0
@@ -100,14 +102,15 @@ leafRanksHelperForProportions rtmVals depth = concatMap (\v -> leafRanksHelper' 
 
 leafRanksHelper' :: RtmValue -> Int -> [(RtmValue, Int)]
 leafRanksHelper' (RtmLeaf _ (RtmProportions rtmVals)) depth = concatMap (\v -> leafRanksHelper' v (depth + 1)) rtmVals
-leafRanksHelper' (RtmNote n) depth = [((RtmNote n), depth)]
-leafRanksHelper' (RtmRest n) depth = [((RtmRest n), depth)]
+leafRanksHelper' (RtmNote n) depth = [(RtmNote n, depth)]
+leafRanksHelper' (RtmRest n) depth = [(RtmRest n, depth)]
 
-tree1 = RtmLeaf 1 (RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4]), RtmRest 3])
-tree2 = RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4]), RtmRest 3]
 
 
 {- 
+>>> tree1 = RtmLeaf 1 (RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4]), RtmRest 3])
+>>> tree2 = RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4]), RtmRest 3]
+
 >>> leafRanks tree1
 [(RtmNote 5,1),(RtmNote 6,2),(RtmRest 4,2),(RtmRest 3,1)]
 >>> leafRanksFromProportions tree2 
@@ -142,6 +145,9 @@ pathLengths val = map length (leafPaths' val)
 
 
 {- 
+>>> tree1 = RtmLeaf 1 (RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4]), RtmRest 3])
+>>> tree2 = RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4]), RtmRest 3]
+
 >>> leafPaths tree1
 [(RtmNote 5,[0]),(RtmNote 6,[1,0]),(RtmRest 4,[1,1]),(RtmRest 3,[2])]
 
@@ -167,8 +173,9 @@ filterValues p (v:vs)
 
 {- 
 tree4 = RtmProportions [RtmNote 5, RtmLeaf 2 (RtmProportions [RtmNote 6, RtmRest 4]), RtmRest 3]
-result3 = aplFilter (\x -> case x of RtmNote n -> n `mod` 2 == 0; _ -> True) tree4 -- Filter even RtmNotes
- -}
+aplFilter (\x -> case x of RtmNote n -> n `mod` 2 == 0; _ -> True) tree4 -- Filter even RtmNotes
+-- RtmProportions [RtmLeaf 2 (RtmProportions [RtmNote 6,RtmRest 4]),RtmRest 3]
+-}
 
 
 aplReduce :: (RtmValue -> RtmValue -> RtmValue) -> RtmProportions -> RtmProportions
@@ -182,7 +189,6 @@ reduceValues f (x:y:rest) = reduceValues f (f x y : reduceValues f rest)
 -- reduceValues _ xs = xs  -- Handle the cases for RtmNote and RtmRest
 
 
--- Example: Sum a list of numbers
 sumList :: [RtmValue] -> RtmValue
 sumList values = case reduceValues (\x y -> RtmNote (getValue x + getValue y)) values of
     [result] -> result
@@ -195,14 +201,14 @@ getValue _ = 0
 
 {- 
 inputList = [RtmNote 1, RtmNote 2, RtmNote 3]
-result = sumList inputList
+sumList inputList
 -- RtmNote 6
- -}
+-}
 
  {- 
  -- Summing all RtmNotes in a sequence
 input1 = RtmProportions [RtmNote 5, RtmNote 3, RtmNote 2]
-result1 = aplReduce (\x y -> RtmNote (getValue x + getValue y)) input1
+aplReduce (\x y -> RtmNote (getValue x + getValue y)) input1
 -- Expected result: RtmProportions [RtmNote 10]
 -- Got: RtmProportions [RtmNote 10]
 
@@ -244,7 +250,8 @@ combineValues (x:y:rest)
     isCombinable _ = False
 {- 
 inputProportions = RtmProportions [RtmNote 5, RtmNote 3, RtmRest 3, RtmRest 2, RtmNote 2]
-resultProportions = combineProportions inputProportions
--- Expected result: RtmProportions [RtmNote 5, RtmNote 3, RtmRest 5, RtmNote 2]
+combineProportions inputProportions
+
 -- RtmProportions [RtmNote 5,RtmNote 3,RtmRest 5,RtmNote 2]
+
  -}
