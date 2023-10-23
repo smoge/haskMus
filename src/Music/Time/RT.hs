134bedb832f8ec06215851cb1e52777fda4054f5
Bernardo Barros <bernardo.barros@protonmail.ch>
2023-10-23 02:24:47 -0400

    Refactored code and added ranked leaf extraction

--- src/Music/Time/RT.hs
+++ src/Music/Time/RT.hs
@@ -1,6 +1,5 @@
-{-# LANGUAGE DataKinds             #-}
-{-# LANGUAGE GADTs                 #-}
-
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs     #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module Music.Time.RT where
@@ -18,7 +17,7 @@ data Component
   | Gap Int
   | Vector Int [Component]
   deriving (Eq, Ord, Show)
-  
+
 -- | This function checks if the Component is valid.
 --
 -- >>> isValidComponent $ Scalar 5
@@ -172,14 +171,12 @@ componentsOfArray example4
 -- []
 
 --  -}
-
 -- | Function to create a `Component` based on a `Shape` and a list of integers.
 fromShapeAndComponents :: Shape -> [Int] -> Maybe Component
-
 -- If the shape is a single Scalar:
 fromShapeAndComponents S (x:_)
-  | x > 0 = Just $ Scalar x  -- Positive value becomes a Scalar.
-  | x < 0 = Just $ Gap (-x)  -- Negative value becomes a Gap with the absolute value.
+  | x > 0 = Just $ Scalar x -- Positive value becomes a Scalar.
+  | x < 0 = Just $ Gap (-x) -- Negative value becomes a Gap with the absolute value.
   | otherwise = Just $ Gap 0 -- Zero can become a Gap of duration zero, or you could use 'Nothing' here if that makes more sense in context.
 -- If the shape is a Vector:
 fromShapeAndComponents (V shps) (n:xs) = do
@@ -197,29 +194,25 @@ fromShapeAndComponents (V shps) (n:xs) = do
       let remaining = drop (length (componentsOfArray [val])) vals
       -- Return the accumulated list of components and the remaining integers.
       return (acc ++ [val], remaining)
-
 -- For other patterns, return Nothing.
 fromShapeAndComponents _ _ = Nothing
 
-
 -- | Function to reconstruct a list of `Component`s from lists of shapes and integers.
 reconstruct :: [Shape] -> [Int] -> [Maybe Component]
-reconstruct [] _ = []  -- Base case: If there are no shapes, return an empty list.
-
+reconstruct [] _ = [] -- Base case: If there are no shapes, return an empty list.
 -- For non-empty list of shapes:
 reconstruct (sh:shps) vals =
   case fromShapeAndComponents sh vals of
     -- If creating a component fails, return Nothing and continue with the rest.
     Nothing -> Nothing : reconstruct shps vals
-    Just val ->
+    Just val
       -- Calculate the integers used by the created component.
+     ->
       let usedVals = componentsOfArray [val]
           -- Remove the used integers.
           remVals = drop (length usedVals) vals
-       in
           -- Return the created component and continue with the remaining shapes and integers.
-          Just val : reconstruct shps remVals
-
+       in Just val : reconstruct shps remVals
 
 -- fromShapeAndComponents :: Shape -> [Int] -> Maybe Component
 -- fromShapeAndComponents S (x:_)
@@ -236,7 +229,6 @@ reconstruct (sh:shps) vals =
 --       let remaining = drop (length (componentsOfArray [val])) vals
 --       return (acc ++ [val], remaining)
 -- fromShapeAndComponents _ _ = Nothing
-
 -- reconstruct :: [Shape] -> [Int] -> [Maybe Component]
 -- reconstruct [] _ = []
 -- reconstruct (sh:shps) vals =
@@ -246,8 +238,7 @@ reconstruct (sh:shps) vals =
 --       let usedVals = componentsOfArray [val]
 --           remVals = drop (length usedVals) vals
 --        in Just val : reconstruct shps remVals
-
--- Test
+{- -- Test
 testArray :: [Component]
 testArray =
   [Scalar 5, Gap 3, Vector 4 [Scalar 2, Gap 3, Vector 3 [Gap 2, Scalar 1]]]
@@ -261,7 +252,7 @@ vals' = componentsOfArray testArray
 reconstructedComponentList :: Maybe [Component]
 reconstructedComponentList = sequence $ reconstruct shapes vals'
 -- Just [Scalar 5,Gap 3,Vector 4 [Scalar 2,Gap 3,Vector 3 [Gap 2,Scalar 1]]]
-
+-}
 {-
 example1 :: Maybe Component
 example1 = fromShapeAndComponents S [5]
@@ -270,6 +261,70 @@ example1 = fromShapeAndComponents S [5]
 example2 :: Maybe Component
 example2 = fromShapeAndComponents (V [S, S]) [4, 2, -3]
 -- Just (Vector 4 [Scalar 2,Gap 3])
+ -}
+data RankedLeaf =
+  RankedLeaf Int Component -- Int represents the rank or depth.
+  deriving (Eq, Ord, Show)
+
+extractRankedLeaves :: Component -> [RankedLeaf]
+extractRankedLeaves = go (-1)
+  where
+    go rank (Scalar n) = [RankedLeaf rank (Scalar n)]
+    go rank (Gap n) = [RankedLeaf rank (Gap n)]
+    go rank (Vector n components)
+      -- Increment the rank for each deeper level and recursively extract leaves.
+     = concatMap (go (rank + 1)) components
+
+-- extractRankedLeaves = go 0
+-- This function extracts the ranks from a single RankedLeaf
+getRank :: RankedLeaf -> Int
+getRank (RankedLeaf rank _) = rank
+
+-- This function takes a list of Component and returns a list of Int (ranks)
+extractRanks :: [Component] -> [Int]
+extractRanks = concatMap (map getRank . extractRankedLeaves)
+-- extractRanks components =
+--   [ rank | RankedLeaf rank _ <- concatMap extractRankedLeaves components ]
+{-
+example3 :: Component
+example3 = Vector 4 [Scalar 2, Gap (-3), Vector 3 [Scalar 2, Gap (-1)]]
+
+ranks = extractRankedLeaves example3
+
+pPrint ranks
+
+[ RankedLeaf 1
+    ( Scalar 2 )
+, RankedLeaf 1
+    ( Gap
+        ( -3 )
+    )
+, RankedLeaf 2
+    ( Scalar 2 )
+, RankedLeaf 2
+    ( Gap
+        ( -1 )
+    )
+]
+
+ -}
+ {-
+simpleVector :: Component
+simpleVector = Vector 3 [Scalar 2, Gap 4, Scalar 6]
+
+nestedVector :: Component
+nestedVector = Vector 7 [Scalar 2, Vector 3 [Scalar 4, Gap 1], Scalar 6]
 
 
+example2 :: [Int]
+example2 = extractRanks [simpleVector]  -- Expected output: [0, 0, 0]
+
+-- Extracting ranks from the nested vector. We should get:
+-- - Scalar 2 at top level => 0
+-- - Vector 3 itself at top level, but it has nested components
+--   - Scalar 4 at depth 1 => 1
+--   - Gap 1 at depth 1 => 1
+-- - Scalar 6 at top level => 0
+example3 :: [Int]
+example3 = extractRanks [nestedVector]  -- Expected output: [0, 1, 1, 0]
  -}
