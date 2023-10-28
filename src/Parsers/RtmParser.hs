191e0ce5e31caceb58d2f35447500acd3b7e1045
Bernardo Barros <bernardo.barros@protonmail.ch>
2023-10-28 05:02:32 -0400

    Refactor RtmParser module and add quickcheck tests

--- src/Parsers/RtmParser.hs
+++ src/Parsers/RtmParser.hs
@@ -1,12 +1,18 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Eta reduce" #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Parsers.RtmParser where
 
 import           Control.Applicative
 import           Data.Either         (fromRight)
+import           Data.Tree
 import qualified Data.Tree           as T
+import           Debug.Trace
 import           Test.HUnit
+import           Test.QuickCheck     hiding (vector)
+
 import           Text.Parsec         hiding ((<|>))
 import           Text.Parsec.String
 
@@ -47,11 +53,25 @@ componentToString (T.Node (Scalar _) _) =
   error "Scalar should not have children"
 componentToString (T.Node (Gap _) _) = error "Gap should not have children"
 
+-- isValidComponent :: Component -> Bool
+-- isValidComponent (T.Node (Scalar n) []) = n > 0
+-- isValidComponent (T.Node (Gap n) [])    = n > 0
+-- isValidComponent (T.Node (Vector n) cs) = n > 0 && all isValidComponent cs
+-- isValidComponent _                      = False
 isValidComponent :: Component -> Bool
-isValidComponent (T.Node (Scalar n) []) = n > 0
-isValidComponent (T.Node (Gap n) [])    = n > 0
-isValidComponent (T.Node (Vector n) cs) = n > 0 && all isValidComponent cs
-isValidComponent _                      = False
+isValidComponent comp = isValidComponentAux comp 0
+  where
+    depthThreshold = 4 -- Adjust this value based on your requirements
+    isValidComponentAux :: Component -> Int -> Bool
+    isValidComponentAux (T.Node (Scalar n) []) _ = n > 0
+    isValidComponentAux (T.Node (Gap n) []) _ = n > 0
+    isValidComponentAux (T.Node (Vector n) cs) depth
+      | depth >= depthThreshold = n > 0 && all isLeaf cs
+      | otherwise = n > 0 && all (`isValidComponentAux` (depth + 1)) cs
+    isValidComponentAux _ _ = False
+    isLeaf (T.Node (Scalar _) []) = True
+    isLeaf (T.Node (Gap _) [])    = True
+    isLeaf _                      = False
 
 componentParser :: Parser Component
 componentParser = try vectorParser <|> try scalarParser <|> gapParser
@@ -127,7 +147,291 @@ tests =
     [testComponentToString, testParseComponentString, testIsValidComponent]
 
 -- Running tests in main
-main :: IO Counts
-main = do
+main3 :: IO Counts
+main3 = do
   putStrLn "Running tests..."
   runTestTT tests
+
+--
+-- QUICKCHECK
+-- Define Arbitrary instances for ComponentLabel and Component
+instance Arbitrary ComponentLabel where
+  arbitrary =
+    oneof [Scalar <$> positiveInt, Gap <$> positiveInt, Vector <$> positiveInt]
+
+arbitraryComponent :: Gen Component
+arbitraryComponent = sized genComponent
+
+genComponent :: Int -> Gen Component
+genComponent 0 = T.Node <$> arbitraryLeaf <*> pure []
+genComponent n
+  | n > 0 =
+    oneof
+      [vectorWithChildren n, arbitraryLeaf >>= \label -> pure (T.Node label [])]
+
+vectorWithChildren :: Int -> Gen Component
+vectorWithChildren n = do
+  label <- Vector <$> positiveInt
+  numChildren <- choose (2, 8)
+  children <- vectorOf numChildren (genComponent (n `div` (numChildren + 1))) -- Distributing remaining size amongst children
+  return (T.Node label children)
+
+arbitraryLeaf :: Gen ComponentLabel
+arbitraryLeaf = oneof [Scalar <$> positiveInt, Gap <$> positiveInt]
+
+positiveInt :: Gen Int
+positiveInt = choose (1, 9)
+
+prop_isValidComponent :: Property
+prop_isValidComponent =
+  forAll arbitraryComponent $ \component ->
+    traceShowId (show component) `seq` isValidComponent component ==> True
+
+isRight :: Either a b -> Bool
+isRight (Right _) = True
+isRight _         = False
+
+main :: IO ()
+main = do
+  verboseCheck prop_isValidComponent
+{-
+t = Node
+    { rootLabel = Vector 2
+    , subForest =
+        [ Node {rootLabel = Scalar 3, subForest = []}
+        , Node {rootLabel = Scalar 9, subForest = []}
+        , Node {rootLabel = Scalar 1, subForest = []}
+        , Node
+            { rootLabel = Vector 4
+            , subForest =
+                [ Node {rootLabel = Gap 8, subForest = []}
+                , Node
+                    { rootLabel = Vector 1
+                    , subForest =
+                        [ Node {rootLabel = Scalar 7, subForest = []}
+                        , Node {rootLabel = Scalar 7, subForest = []}
+                        ]
+                    }
+                , Node {rootLabel = Gap 1, subForest = []}
+                ]
+            }
+        , Node
+            { rootLabel = Vector 2
+            , subForest =
+                [ Node {rootLabel = Scalar 3, subForest = []}
+                , Node
+                    { rootLabel = Vector 5
+                    , subForest =
+                        [ Node {rootLabel = Gap 6, subForest = []}
+                        , Node {rootLabel = Scalar 6, subForest = []}
+                        , Node {rootLabel = Gap 2, subForest = []}
+                        , Node {rootLabel = Scalar 5, subForest = []}
+                        , Node {rootLabel = Gap 2, subForest = []}
+                        , Node {rootLabel = Scalar 2, subForest = []}
+                        , Node {rootLabel = Scalar 1, subForest = []}
+                        ]
+                    }
+                , Node {rootLabel = Gap 9, subForest = []}
+                , Node
+                    { rootLabel = Vector 7
+                    , subForest =
+                        [ Node {rootLabel = Scalar 5, subForest = []}
+                        , Node {rootLabel = Scalar 9, subForest = []}
+                        ]
+                    }
+                , Node {rootLabel = Scalar 2, subForest = []}
+                , Node
+                    { rootLabel = Vector 5
+                    , subForest =
+                        [ Node {rootLabel = Gap 8, subForest = []}
+                        , Node {rootLabel = Gap 9, subForest = []}
+                        , Node {rootLabel = Scalar 5, subForest = []}
+                        ]
+                    }
+                , Node
+                    { rootLabel = Vector 1
+                    , subForest =
+                        [ Node {rootLabel = Gap 9, subForest = []}
+                        , Node {rootLabel = Scalar 4, subForest = []}
+                        , Node {rootLabel = Scalar 2, subForest = []}
+                        , Node {rootLabel = Gap 4, subForest = []}
+                        ]
+                    }
+                ]
+            }
+        , Node
+            { rootLabel = Vector 6
+            , subForest =
+                [ Node {rootLabel = Scalar 5, subForest = []}
+                , Node
+                    { rootLabel = Vector 7
+                    , subForest =
+                        [ Node {rootLabel = Scalar 7, subForest = []}
+                        , Node {rootLabel = Scalar 2, subForest = []}
+                        , Node {rootLabel = Gap 7, subForest = []}
+                        , Node {rootLabel = Scalar 1, subForest = []}
+                        , Node {rootLabel = Scalar 5, subForest = []}
+                        ]
+                    }
+                , Node {rootLabel = Gap 1, subForest = []}
+                , Node
+                    { rootLabel = Vector 3
+                    , subForest =
+                        [ Node {rootLabel = Scalar 3, subForest = []}
+                        , Node {rootLabel = Gap 3, subForest = []}
+                        , Node {rootLabel = Scalar 3, subForest = []}
+                        ]
+                    }
+                , Node {rootLabel = Gap 5, subForest = []}
+                , Node {rootLabel = Scalar 8, subForest = []}
+                , Node {rootLabel = Gap 2, subForest = []}
+                , Node
+                    { rootLabel = Vector 4
+                    , subForest =
+                        [ Node {rootLabel = Scalar 8, subForest = []}
+                        , Node {rootLabel = Gap 3, subForest = []}
+                        , Node {rootLabel = Gap 1, subForest = []}
+                        , Node {rootLabel = Scalar 4, subForest = []}
+                        ]
+                    }
+                ]
+            }
+        , Node {rootLabel = Gap 3, subForest = []}
+        , Node
+            { rootLabel = Vector 6
+            , subForest =
+                [ Node {rootLabel = Scalar 6, subForest = []}
+                , Node {rootLabel = Scalar 8, subForest = []}
+                , Node {rootLabel = Gap 3, subForest = []}
+                , Node {rootLabel = Gap 5, subForest = []}
+                ]
+            }
+        ]
+    }
+
+
+printTree t
+
+
+Vector 2
+|
++- Scalar 3
+|
++- Scalar 9
+|
++- Scalar 1
+|
++- Vector 4
+|  |
+|  +- Gap 8
+|  |
+|  +- Vector 1
+|  |  |
+|  |  +- Scalar 7
+|  |  |
+|  |  `- Scalar 7
+|  |
+|  `- Gap 1
+|
++- Vector 2
+|  |
+|  +- Scalar 3
+|  |
+|  +- Vector 5
+|  |  |
+|  |  +- Gap 6
+|  |  |
+|  |  +- Scalar 6
+|  |  |
+|  |  +- Gap 2
+|  |  |
+|  |  +- Scalar 5
+|  |  |
+|  |  +- Gap 2
+|  |  |
+|  |  +- Scalar 2
+|  |  |
+|  |  `- Scalar 1
+|  |
+|  +- Gap 9
+|  |
+|  +- Vector 7
+|  |  |
+|  |  +- Scalar 5
+|  |  |
+|  |  `- Scalar 9
+|  |
+|  +- Scalar 2
+|  |
+|  +- Vector 5
+|  |  |
+|  |  +- Gap 8
+|  |  |
+|  |  +- Gap 9
+|  |  |
+|  |  `- Scalar 5
+|  |
+|  `- Vector 1
+|     |
+|     +- Gap 9
+|     |
+|     +- Scalar 4
+|     |
+|     +- Scalar 2
+|     |
+|     `- Gap 4
+|
++- Vector 6
+|  |
+|  +- Scalar 5
+|  |
+|  +- Vector 7
+|  |  |
+|  |  +- Scalar 7
+|  |  |
+|  |  +- Scalar 2
+|  |  |
+|  |  +- Gap 7
+|  |  |
+|  |  +- Scalar 1
+|  |  |
+|  |  `- Scalar 5
+|  |
+|  +- Gap 1
+|  |
+|  +- Vector 3
+|  |  |
+|  |  +- Scalar 3
+|  |  |
+|  |  +- Gap 3
+|  |  |
+|  |  `- Scalar 3
+|  |
+|  +- Gap 5
+|  |
+|  +- Scalar 8
+|  |
+|  +- Gap 2
+|  |
+|  `- Vector 4
+|     |
+|     +- Scalar 8
+|     |
+|     +- Gap 3
+|     |
+|     +- Gap 1
+|     |
+|     `- Scalar 4
+|
++- Gap 3
+|
+`- Vector 6
+   |
+   +- Scalar 6
+   |
+   +- Scalar 8
+   |
+   +- Gap 3
+   |
+   `- Gap 5
+ -}
