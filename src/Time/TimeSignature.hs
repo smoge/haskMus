163433749084d32d500576f1aa6df8ef268a279b
Bernardo Barros <bernardo.barros@protonmail.ch>
2023-11-15 10:03:29 -0500

    Refactor TimeSignature module and update stack.yaml

--- src/Time/TimeSignature.hs
+++ src/Time/TimeSignature.hs
@@ -2,10 +2,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-{- | This module defines a representation of musical time signatures and
- provides functionality to convert between 'TimeSignature' and 'Dur'
- (duration) types.
--}
+
 module Time.TimeSignature (
     TimeSignature (..),
     time,
@@ -16,19 +13,14 @@ module Time.TimeSignature (
     HasDur (..),
 ) where
 
-import Data.Bits ((.&.))
 import Data.Default
 import Data.Ratio
 import Time.Dur
 
-{- | A data type representing a time signature in music, which specifies the
- number of beats in a measure ('upper') and the note value that constitutes
- one beat ('lower').
--}
 data TimeSignature = TimeSignature
-    { -- | The number of beats in a measure.
+    { 
       upper :: Integer,
-      -- | The note value that constitutes one beat.
+      
       lower :: Integer
     }
     deriving (Eq, Ord)
@@ -40,22 +32,13 @@ instance Show TimeSignature where
 infixr 7 //
 
 {- | Infix constructor for 'TimeSignature'.
-
- ==== __Examples__
-
  >>> 4 // 4
  TimeSignature 4/4
-
- >>> 6 // 8
- TimeSignature 6/8
 -}
 (//) :: Integer -> Integer -> TimeSignature
 n // d = time n d
 
 {- | Creates a 'TimeSignature' from two integers.
-
- ==== __Examples__
-
  >>> time 3 4
  TimeSignature 3/4
 -}
@@ -73,13 +56,10 @@ instance HasDur TimeSignature where
     toDur :: TimeSignature -> Dur
     toDur = timeSigToDur
 
-    setDur :: TimeSignature -> Dur -> TimeSignature
+    setDur ::  TimeSignature -> Dur -> TimeSignature
     setDur ts d = durToTimeSig (denominator $ unDur $ toDur ts) d
 
 {- | Converts a 'TimeSignature' to a 'Dur'.
-
- ==== __Examples__
-
  >>> timeSigToDur (3 // 4)
  Dur (3 % 4)
 
@@ -91,14 +71,13 @@ timeSigToDur (TimeSignature n d) = Dur $ n % d
 
 {- | Converts a 'Dur' to a 'TimeSignature' with a preferred denominator.
 
- ==== __Examples__
-
  >>> durToTimeSig 4 (Dur (1 % 2))
  TimeSignature 2/4
 
  >>> durToTimeSig 8 (Dur (3 % 4))
  TimeSignature 6/8
 -}
+
 durToTimeSig :: (Integral a) => a -> Dur -> TimeSignature
 durToTimeSig preferredDenominator d
     | denominatorRatio == preferredDenominator' =
@@ -120,6 +99,7 @@ durToTimeSig preferredDenominator d
  TimeSignature 4/8
  TimeSignature 3/16
 -}
+
 durToTimeSig8 :: Dur -> TimeSignature
 durToTimeSig8 durat
     | den <= 8 = durToTimeSig (8 :: Integer) durat
@@ -127,8 +107,3 @@ durToTimeSig8 durat
     where
         den = (denominator . unDur) durat
 
-powersOfTwo :: [Integer]
-powersOfTwo = fmap (2 ^) ([0 .. 10] :: [Integer])
-
-isPowerOfTwo :: Integer -> Bool
-isPowerOfTwo n = n > 0 && (n .&. (n - 1)) == 0
