8fc488bfde1ec4b00977a753ce88384d9a814a36
Bernardo Barros <bernardo.barros@protonmail.ch>
2023-11-21 01:16:51 -0500

    added "ToXML" to the build-depends section

--- src/Pitch/Accidental.hs
+++ src/Pitch/Accidental.hs
@@ -29,7 +29,6 @@ import Data.String
 import qualified Data.Text as T
 import Language.Haskell.TH.Syntax (Lift)
 
-
 data Accidental
     = DoubleFlat
     | ThreeQuartersFlat
@@ -43,7 +42,6 @@ data Accidental
     | Custom Rational
     deriving (Eq, Ord, Show, Lift, Data)
 
-
 -- instance Lift Accidental
 
 {- |
@@ -53,28 +51,23 @@ data Accidental
  >>> map flattenS allAccidentals
  [ThreeQuartersFlat,Flat,QuarterFlat,Natural,QuarterSharp,Sharp,ThreeQuartersSharp,DoubleSharp,Custom (5 % 2)]
  [Flat,QuarterFlat,Natural,QuarterSharp,Sharp,ThreeQuartersSharp,DoubleSharp,Custom (5 % 2),Custom (3 % 1)]
- [Custom ((-5) % 2),DoubleFlat,ThreeQuartersFlat,Flat,QuarterFlat,Natural,QuarterSharp,Sharp,ThreeQuartersSharp]
+ [Custom ((-5) % 2),DoubleFlat,ThreeQuartersFlat,Flat,QuarterFlat,Natural,QuarterSharp,Sharp,ThreeQuartersSharp]l
  [Custom ((-3) % 1),Custom ((-5) % 2),DoubleFlat,ThreeQuartersFlat,Flat,QuarterFlat,Natural,QuarterSharp,Sharp]
 -}
 sharpenQ :: Accidental -> Accidental
 sharpenQ acc = modifyAccidental acc (+ (1 / 2))
 
-
 sharpenS :: Accidental -> Accidental
 sharpenS acc = modifyAccidental acc (+ 1)
-
 
 flattenQ :: Accidental -> Accidental
 flattenQ acc = modifyAccidental acc (+ ((-1) % 2))
 
-
 flattenS :: Accidental -> Accidental
 flattenS acc = modifyAccidental acc (+ (-1))
 
-
 accidentalCompare :: Accidental -> Accidental -> Ordering
 accidentalCompare = comparing accidentalToSemitones
-
 
 {- -----------------------------------------------------------------------------
 
@@ -88,71 +81,55 @@ sortBy accidentalCompare scrambled1
 class IsAccidental a where
     toAccidental :: a -> Accidental
 
-
 data SomeAccidental
     = forall accidental.
         (IsAccidental accidental) =>
       SomeAccidental accidental
 
-
 instance IsAccidental SomeAccidental where
     toAccidental :: SomeAccidental -> Accidental
     toAccidental (SomeAccidental acc) = toAccidental acc
-
 
 instance IsAccidental Accidental where
     toAccidental :: Accidental -> Accidental
     toAccidental = id
 
-
 instance IsAccidental Rational where
     toAccidental :: Rational -> Accidental
     toAccidental = semitonesToAccidental
 
-
 class AccClass (accNme :: Accidental) where
     sayAccidental :: String
-
 
 instance Show SomeAccidental where
     show = show . toAccidental
 
-
 instance AccClass DoubleFlat where
     sayAccidental = "ff"
-
 
 instance AccClass Flat where
     sayAccidental = "f"
 
-
 instance AccClass Natural where
     sayAccidental = "n"
-
 
 instance AccClass QuarterFlat where
     sayAccidental = "qf"
 
-
 instance AccClass QuarterSharp where
     sayAccidental = "qs"
-
 
 instance AccClass Sharp where
     sayAccidental = "s"
 
-
 instance AccClass ThreeQuartersSharp where
     sayAccidental = "qss"
-
 
 instance AccClass ThreeQuartersFlat where
     sayAccidental = "qsf"
 
-
 instance AccClass DoubleSharp where
     sayAccidental = "ss"
-
 
 -- acidente1 :: Accidental
 -- acidente1 = "ff"
@@ -193,7 +170,6 @@ instance Num Accidental where
     negate (Custom r) = (Custom (-r))
     negate a = toAccidental $ negate (accidentalToSemitones a)
 
-
 instance Enum Accidental where
     toEnum n = case n of
         0 -> DoubleFlat
@@ -207,7 +183,6 @@ instance Enum Accidental where
         8 -> DoubleSharp
         _ -> error "toEnum: Out of bounds for Accidental"
 
-
     fromEnum acc = case acc of
         DoubleFlat -> 0
         ThreeQuartersFlat -> 1
@@ -220,23 +195,19 @@ instance Enum Accidental where
         DoubleSharp -> 8
         Custom _ -> error "fromEnum: Custom accidental doesn't have a fixed enumeration"
 
-
     succ :: Accidental -> Accidental
     succ acc
         | acc == maxBound = error "succ: tried to take succ of maxBound"
         | otherwise = sharpenQ acc
-
 
     pred :: Accidental -> Accidental
     pred acc
         | acc == minBound = error "pred: tried to take pred of minBound"
         | otherwise = flattenQ acc
 
-
 instance Bounded Accidental where
     minBound = DoubleFlat
     maxBound = DoubleSharp
-
 
 -- | Converts an accidental to its corresponding semitone offset as a rational number.
 accidentalToSemitones :: Accidental -> Rational
@@ -251,7 +222,6 @@ accidentalToSemitones ThreeQuartersSharp = 3 % 2
 accidentalToSemitones DoubleSharp = 2
 accidentalToSemitones (Custom r) = r
 
-
 -- | Converts a semitone offset to the corresponding accidental.
 semitonesToAccidental :: Rational -> Accidental
 semitonesToAccidental r
@@ -265,7 +235,6 @@ semitonesToAccidental r
     | r == 3 % 2 = ThreeQuartersSharp
     | r == 2 % 1 = DoubleSharp
     | otherwise = (Custom r)
-
 
 {- | Converts an accidental to its corresponding LilyPond representation.
  >>>  map accToLily allAccidentals == map  T.pack ["ff","tqf","f","qf","","qs","s","tqs","ss"]
@@ -282,7 +251,6 @@ accToLily Sharp = T.pack "s"
 accToLily ThreeQuartersSharp = T.pack "tqs"
 accToLily DoubleSharp = T.pack "ss"
 accToLily (Custom r) = T.pack $ show r
-
 
 instance Read Accidental where
     readsPrec _ value =
@@ -305,7 +273,6 @@ instance Read Accidental where
             "quarterflat" -> [(QuarterFlat, "")]
             "semiflat" -> [(QuarterFlat, "")]
             _ -> error $ "Invalid Accidental string: " <> value
-
 
 -- >>> map (fromString @Accidental) ["ff","tqf","f","qf","","qs","s","tqs","ss"]
 -- [DoubleFlat,ThreeQuartersFlat,Flat,QuarterFlat,Natural,QuarterSharp,Sharp,ThreeQuartersSharp,DoubleSharp]
@@ -339,7 +306,6 @@ instance IsString Accidental where
         | "custom " `isPrefixOf` str = Custom (read (drop 7 str) :: Rational)
         | otherwise = error $ "Invalid Accidental string: " <> str
 
-
 {- | Modify the accidental by applying a function to its semitone value. Returns the modified accidental.
  >>> modifyAccidental Sharp (*2)
  DoubleSharp
@@ -360,11 +326,9 @@ modifyAccidental acc f =
                 | r == 2 -> DoubleSharp
                 | otherwise -> (Custom r)
 
-
 -- | Checks whether an accidental is equal to its corresponding semitone offset.
 checkAccidental :: Accidental -> Bool
 checkAccidental acc = semitonesToAccidental (accidentalToSemitones acc) == acc
-
 
 {- | Modifies an accidental by a given delta (Rational).
 
@@ -389,10 +353,8 @@ addAccidental acc delta
         currentSemitone = accidentalToSemitones acc
         newSemitone = currentSemitone + delta
 
-
 invertAccidental :: Accidental -> Accidental
 invertAccidental = negate
-
 
 allAccidentals :: [Accidental]
 allAccidentals =
@@ -406,7 +368,6 @@ allAccidentals =
       ThreeQuartersSharp,
       DoubleSharp
     ]
-
 
 allSemitones :: [Rational]
 allSemitones = fmap accidentalToSemitones allAccidentals
---
+++ src/ToXML.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module ToXML where
+
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
+-- import qualified Data.Text.Lazy.IO as TLIO
+import Text.XML
+import Text.XML.Writer
+
+
+-- Serializing is easy!
+data ExampleADT
+  = NullaryExample
+  | UnaryExample String
+  | RecordExample
+      { earFoo :: Int,
+        earBar :: Maybe Bool,
+        earBaz :: [Float]
+      }
+
+
+instance ToXML ExampleADT where
+  toXML NullaryExample = empty
+  toXML (UnaryExample s) = element "unary" $ content (T.pack s)
+  toXML (RecordExample {earFoo = foo, earBar = bar, earBaz = baz}) =
+    element "record" $ do
+      element "foo" $ toXML foo
+      element "bar" $ toXML bar
+      element "baz" $ many "fnord" baz
+
+
+-- | Create a MusicXML Document with a specific DOCTYPE
+documentMusicXML ::
+  -- | Root node name
+  Name ->
+  -- | Attributes
+  [(Name, T.Text)] ->
+  -- | Contents
+  XML ->
+  Document
+documentMusicXML name attrs children =
+  Document
+    { documentPrologue =
+        Prologue
+          { prologueBefore = [],
+            prologueDoctype =
+              Just
+                $ Doctype
+                  "score-partwise"
+                  ( Just
+                      $ PublicID
+                        "-//Recordare//DTD MusicXML 4.0 Partwise//EN"
+                        "http://www.musicxml.org/dtds/partwise.dtd"
+                  ),
+            prologueAfter = []
+          },
+      documentRoot = Element name (M.fromList attrs) (render children),
+      documentEpilogue = []
+    }
+
+
+main :: IO ()
+main = do
+  let musicXmlDoc =
+        documentMusicXML
+          (Name "score-partwise" Nothing Nothing)
+          [("version", "4.0")]
+          ( do
+              elementA "part-list" [] $ do
+                elementA "score-part" [("id", "P1")] $ do
+                  element "part-name" $ content "Music"
+              elementA "part" [("id", "P1")] $ do
+                elementA "measure" [("number", "1")] $ do
+                  element "attributes" $ do
+                    element "divisions" $ content "1"
+                    element "key" $ do
+                      element "fifths" $ content "0"
+                    element "time" $ do
+                      element "beats" $ content "4"
+                      element "beat-type" $ content "4"
+                    element "clef" $ do
+                      element "sign" $ content "G"
+                      element "line" $ content "2"
+                  element "note" $ do
+                    element "pitch" $ do
+                      element "step" $ content "C"
+                      element "octave" $ content "4"
+                    element "duration" $ content "4"
+                    element "type" $ content "whole"
+          )
+  -- TLIO.putStrLn $ renderText def musicXmlDoc
+  pprint musicXmlDoc
