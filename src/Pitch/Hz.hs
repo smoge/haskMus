236a614b7dc0ed7f5d05c9849733481c0d6d8e76
Bernardo Barros <bernardo.barros@protonmail.ch>
2024-01-31 04:06:31 -0500

    Refactor pitch conversion functions and add MIDI note conversion

--- src/Pitch/Hz.hs
+++ src/Pitch/Hz.hs
@@ -3,8 +3,8 @@ module Pitch.Hz where
 -- | The Pitch.Hz module provides types and functions to work with musical pitches in terms of semitones and Hertz.
 
 -- `newtype` wrappers for `Semitone` and `Hz` provide type safety.
-newtype Semitone = Semitone {unSemitone :: Double} deriving (Eq, Num, Ord)
-newtype Hz = Hz {unHz :: Double} deriving (Eq, Num, Ord)
+newtype Semitone = Semitone { unSemitone :: Double } deriving (Eq, Num, Ord)
+newtype Hz = Hz { unHz :: Double } deriving (Eq, Num, Ord)
 
 instance Show Semitone where
   show (Semitone n) = show n <> " Semitone(s)"
@@ -12,47 +12,44 @@ instance Show Semitone where
 instance Show Hz where
   show (Hz hz) = show hz <> " Hz"
 
-
--- | Standard pitch (A4) definition in Hertz.
--- 
--- >>> pitchStandard
--- 440.0 Hz
+-- Standard pitch (A4) definition in Hertz.
 pitchStandard :: Hz
 pitchStandard = Hz 440.0
 
--- | Convert a `Semitone` or `Double` representing semitones to `Hz`.
--- Works for both integral and fractional semitones.
---
--- >>> semitoneToHz 12
--- 880.0 Hz
+middleCinHz :: Hz
+middleCinHz = Hz 261.625565301
+
+
+-- Convert a `Semitone` or `Double` representing semitones to `Hz`.
 semitoneToHz :: Double -> Hz
-semitoneToHz n = pitchStandard * Hz (2 ** (n / 12.0))
+semitoneToHz n = Hz $ unHz middleCinHz * 2 ** (n / 12.0)
 
--- | Convert a frequency in `Hz` to its equivalent in semitones relative to `pitchStandard`.
--- Calculated using the logarithm base 2.
---
--- >>> hzToDouble (Hz 880.0)
--- 12.0
+-- Convert a frequency in `Hz` to its equivalent in semitones relative to `middleCinHz`.
 hzToDouble :: Hz -> Double
-hzToDouble (Hz hz) = 12.0 * logBase 2 (hz / unHz pitchStandard)
+hzToDouble (Hz hz) = 12.0 * logBase 2 (hz / unHz middleCinHz)
 
--- | Use `hzToDouble` to convert Hz to `Semitone`.
---
--- >>> hzToSemitone (Hz 880.0)
--- 12.0 Semitone(s)
+-- Use `unHz` to convert Hz to `Semitone` for consistency.
 hzToSemitone :: Hz -> Semitone
 hzToSemitone = Semitone . hzToDouble
 
--- | Directly retrieve the underlying `Double` from a `Semitone`.
---
--- >>> semitoneToDouble (Semitone 12.0)
--- 12.0
-semitoneToDouble :: Semitone -> Double
-semitoneToDouble (Semitone n) = n
+-- MIDI note number to Hz
+midiToHz :: Double -> Hz
+midiToHz n = Hz $ 2 ** ((n - 69) / 12) * 440
 
--- | Wrap a `Double` as a `Semitone`.
---
--- >>> doubleToSemitone 12.0
--- 12.0 Semitone(s)
-doubleToSemitone :: Double -> Semitone
-doubleToSemitone = Semitone
+-- Hz to MIDI note number
+hzToMidi :: Hz -> Double
+hzToMidi (Hz hz) = 12 * logBase 2 (hz / 440) + 69
+
+-- Convert a `Semitone` to its equivalent in MIDI note number.
+semitoneToMidi :: Semitone -> Double
+semitoneToMidi = hzToMidi . semitoneToHz . unSemitone
+
+-- Function to round a `Double` to a specified number of decimal places.
+roundTo :: Int -> Double -> Double
+roundTo n f = fromInteger (round $ f * (10^n)) / (10.0^^n)
+
+-- Example usage with rounding: Convert Hz to MIDI and round to 2 decimal places.
+-- >>> roundHzToMidi middleCinHz
+-- 60.0
+roundHzToMidi :: Hz -> Double
+roundHzToMidi = roundTo 5 . hzToMidi
