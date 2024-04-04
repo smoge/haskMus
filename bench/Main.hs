8c583945a419a68d0bbd378770b0fdc073b28665
Bernardo Barros <bernardo.barros@protonmail.ch>
2024-04-04 11:06:05 -0400

    Add benchmark for parsePitches function

--- bench/Main.hs
+++ bench/Main.hs
@@ -3,47 +3,10 @@
 module Main where
 
 import Criterion.Main
-import Pitch.Pitch
 import Pitch.Accidental
+import Pitch.Parser
+import Pitch.Pitch
 
-{-
--- Optimized Fibonacci function using iteration.
-fib :: Integer -> Integer
-fib n
-  | n < 0 = error "negative!"
-  | n == 0 = 0
-  | n == 1 = 1
-  | otherwise = go n 0 1
-  where
-    go 0 a _ = a
-    go 1 _ b = b
-    go k a b = go (k - 1) b (a + b)
-
-fibOld :: Integer -> Integer
-fibOld m
-  | m < 0 = error "negative!"
-  | otherwise = go m
-  where
-    go 0 = 0
-    go 1 = 1
-    go n = go (n - 1) + go (n - 2)
-
-main2 :: IO ()
-main2 =
-  defaultMain
-    [ bgroup
-        "fib"
-        [ bench "1" $ whnf fib 1,
-          bench "5" $ whnf fib 5,
-          bench "9" $ whnf fib 9
-        ],
-      bgroup
-        "fibOld"
-        [ bench "fibOld 1" $ whnf fibOld 1,
-          bench "fibOld 5" $ whnf fibOld 5,
-          bench "fibOld 9" $ whnf fibOld 9
-        ]
-    ] -}
 
 -- Benchmark the pitchToFloat function
 benchmarkPitchToFloat :: Benchmark
@@ -54,10 +17,7 @@ benchmarkPitchToFloat =
       bench "D QuarterSharp Octave 8" $ whnf pitchToFloat (Pitch D QuarterSharp (Octave 8)),
       bench "E Flat Octave 0" $ whnf pitchToFloat (Pitch E Flat (Octave 0)),
       bench "F Natural Octave 0" $ whnf pitchToFloat (Pitch F Natural (Octave 10))
-
     ]
-
-
 
 benchmarkPcToRational :: Benchmark
 benchmarkPcToRational =
@@ -69,10 +29,21 @@ benchmarkPcToRational =
       bench "B Flat" $ whnf pcToRational (PitchClass B Flat)
     ]
 
+{-
+parsePitches "cqs' cqf,  gqs''" `shouldBe` Right [Pitch C QuarterSharp (Octave 5), Pitch C QuarterFlat (Octave 3), Pitch G QuarterSharp (Octave 6)] -}
+
+benchmarkParsePitches :: Benchmark
+benchmarkParsePitches =
+  bgroup
+    "parsePitches"
+    [ bench "c d e f d" $ whnf parsePitches "c d e f d",
+      bench "cqs' cqf,  gqs''" $ whnf parsePitches "cqs' cqf,  gqs''"
+    ]
 
 main :: IO ()
 main =
   defaultMain
     [ benchmarkPitchToFloat,
-      benchmarkPcToRational
+      benchmarkPcToRational,
+      benchmarkParsePitches
     ]
