853210fd2c430f4dd30a2c2f94aeef21ae255a33
Bernardo Barros <bernardo.barros@protonmail.ch>
2024-04-04 11:24:32 -0400

    Update Pitch.Parser.hs

--- src/Pitch/Parser.hs
+++ src/Pitch/Parser.hs
@@ -5,23 +5,14 @@
 module Pitch.Parser where
 
 import Pitch.Accidental
-  ( Accidental (Flat, Natural, QuarterFlat, QuarterSharp, Sharp),
-  )
 import Pitch.Pitch
 import Text.Parsec
-  ( ParseError,
-    char,
-    many,
-    parse,
-    sepEndBy,
-    spaces,
-    string,
-    try,
-    (<|>),
-  )
-import Text.Parsec.String (Parser)
+import qualified Text.Parsec as P
+
+import Text.Parsec.String
 
 -- Consume spaces before and after the parser.
+{-# INLINE spaced #-}
 spaced :: Parser a -> Parser a
 spaced p = spaces *> p <* spaces
 
@@ -62,6 +53,46 @@ pitchClassParser =
     <|> try (string "a" >> pure (PitchClass A Natural))
     <|> try (string "b" >> pure (PitchClass B Natural))
 
+pitchClassParser' :: Parser PitchClass
+pitchClassParser' =
+  P.choice
+    [ P.string "cqs" *> pure (PitchClass C QuarterSharp),
+      P.string "cqf" *> pure (PitchClass C QuarterFlat),
+      P.string "dqf" *> pure (PitchClass D QuarterFlat),
+      P.string "dqs" *> pure (PitchClass D QuarterSharp),
+      P.string "eqf" *> pure (PitchClass E QuarterFlat),
+      P.string "eqs" *> pure (PitchClass E QuarterSharp),
+      P.string "fqs" *> pure (PitchClass F QuarterSharp),
+      P.string "fqf" *> pure (PitchClass F QuarterFlat),
+      P.string "gqf" *> pure (PitchClass G QuarterFlat),
+      P.string "gqs" *> pure (PitchClass G QuarterSharp),
+      P.string "aqf" *> pure (PitchClass A QuarterFlat),
+      P.string "aqs" *> pure (PitchClass A QuarterSharp),
+      P.string "bqf" *> pure (PitchClass B QuarterFlat),
+      P.string "bqs" *> pure (PitchClass B QuarterSharp),
+      P.string "cf" *> pure (PitchClass C Flat),
+      P.string "df" *> pure (PitchClass D Flat),
+      P.string "ef" *> pure (PitchClass E Flat),
+      P.string "ff" *> pure (PitchClass F Flat),
+      P.string "gf" *> pure (PitchClass G Flat),
+      P.string "af" *> pure (PitchClass A Flat),
+      P.string "bf" *> pure (PitchClass B Flat),
+      P.string "cs" *> pure (PitchClass C Sharp),
+      P.string "ds" *> pure (PitchClass D Sharp),
+      P.string "es" *> pure (PitchClass E Sharp),
+      P.string "fs" *> pure (PitchClass F Sharp),
+      P.string "gs" *> pure (PitchClass G Sharp),
+      P.string "as" *> pure (PitchClass A Sharp),
+      P.string "bs" *> pure (PitchClass B Sharp),
+      P.string "c" *> pure (PitchClass C Natural),
+      P.string "d" *> pure (PitchClass D Natural),
+      P.string "e" *> pure (PitchClass E Natural),
+      P.string "f" *> pure (PitchClass F Natural),
+      P.string "g" *> pure (PitchClass G Natural),
+      P.string "a" *> pure (PitchClass A Natural),
+      P.string "b" *> pure (PitchClass B Natural)
+    ]
+
 octaveParser :: Parser Octave
 octaveParser = do
   upOctaves <- many (char '\'')
@@ -76,7 +107,7 @@ parsePitches :: String -> Either ParseError [Pitch]
 parsePitches = parse pitchesParser ""
 
 pitchParser :: Parser Pitch
-pitchParser = spaced $ do
+pitchParser = do
   pc <- pitchClassParser
   mkPitch' pc <$> octaveParser
 
