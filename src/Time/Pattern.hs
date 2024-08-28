0231b4bb9805865635bc7edfd8326522736b4228
Bernardo Barros <bernardobarros@gmail.com>
2024-08-28 07:17:18 -0400

    r Time.Pattern module for SuperCollider-inspired patterns Updated the Stream functor and applicative instances

--- src/Time/Pattern.hs
+++ src/Time/Pattern.hs
@@ -23,7 +23,7 @@ type Duration = Double
 
 -- | An Event represents a single musical event with a start time, duration, and a map of parameters.
 -- This is similar to SuperCollider's Event concept.
-data Event = Event 
+data Event = Event
     { eTime :: Time        -- ^ The start time of the event
     , eDuration :: Duration -- ^ The duration of the event
     , eParameters :: Map String Double  -- ^ Parameters like frequency, amplitude, etc.
@@ -51,25 +51,25 @@ class IsPattern p where
 instance Functor Stream where
     fmap f (Stream s) = Stream $ \t -> do
         (x, s') <- s t
-        return (f x, fmap f s')
+        pure (f x, fmap f s')
 
 -- Applicative instance for Stream allows combining streams.
 instance Applicative Stream where
-    pure x = Stream $ \_ -> return (x, pure x)
+    pure x = Stream $ \_ -> pure (x, pure x)
     (Stream sf) <*> (Stream sx) = Stream $ \t -> do
         (f, sf') <- sf t
         (x, sx') <- sx t
-        return (f x, sf' <*> sx')
+        pure (f x, sf' <*> sx')
 
 -- IsPattern instance for Pattern defines how each pattern type is converted to a stream.
 instance IsPattern Pattern where
     -- PSeq creates a sequence of patterns, optionally repeating
-    asStream (PSeq patterns repeat_) = Stream $ \t -> 
+    asStream (PSeq patterns repeat_) = Stream $ \t ->
         let loop [] = if repeat_ then loop patterns else error "PSeq exhausted"
             loop (p:ps) = do
                 (x, s) <- runStream (asStream p) t
-                return (x, Stream $ \t' -> 
-                    if t' == t 
+                pure (x, Stream $ \t' ->
+                    if t' == t
                         then runStream s t'
                         else loop ps)
         in loop patterns
@@ -82,12 +82,12 @@ instance IsPattern Pattern where
     -- PWhite generates random values within a given range
     asStream (PWhite min_ max_) = Stream $ \_ -> do
         x <- randomRIO (min_, max_)
-        return (x, asStream (PWhite min_ max_))
+        pure (x, asStream (PWhite min_ max_))
 
     -- PGeom generates a geometric series
     asStream (PGeom start grow len) = Stream $ \t ->
         let x = start * (grow ^ (floor t `mod` len))
-        in return (x, asStream (PGeom start grow len))
+        in pure (x, asStream (PGeom start grow len))
 
     -- PMap applies a function to a pattern
     asStream (PMap f p) = fmap f (asStream p)
@@ -96,25 +96,25 @@ instance IsPattern Pattern where
     asStream (PBind paramStreams) = Stream $ \t -> do
         (params, paramStreams') <- runParamStreams paramStreams t
         let dur = Map.findWithDefault 0 "dur" params
-            event = Event 
+            event = Event
                 { eTime = t
                 , eDuration = dur
                 , eParameters = params
                 }
-        return (event, asStream (PBind paramStreams'))
+        pure (event, asStream (PBind paramStreams'))
 
 -- | Runs all parameter streams for a given time, returning the current values and updated streams.
 runParamStreams :: Map String (Stream Double) -> Time -> IO (Map String Double, Map String (Stream Double))
 runParamStreams streams t = do
     let runStream' k s = do
             (v, s') <- runStream s t
-            return (k, (v, s'))
+            pure (k, (v, s'))
     results <- mapM (uncurry runStream') (Map.toList streams)
     let params = Map.fromList [(k, v) | (k, (v, _)) <- results]
         streams' = Map.fromList [(k, s') | (k, (_, s')) <- results]
-    return (params, streams')
+    pure (params, streams')
 
--- Utility functions to create different types of patterns
+
 
 -- | Creates a sequence pattern.
 pseq :: [Pattern a] -> Bool -> Pattern a
@@ -152,21 +152,21 @@ class RunPattern p where
 instance RunPattern (Pattern Event) where
     runPattern steps p = go steps (asStream p) 0
       where
-        go 0 _ _ = return []
+        go 0 _ _ = pure []
         go n s t = do
           (event@Event{eDuration = dur}, s') <- runStream s t
           events <- go (n-1) s' (t + dur)
-          return (event : events)
+          pure (event : events)
 
 -- RunPattern instance for Pattern Double
 instance RunPattern (Pattern Double) where
     runPattern steps p = go steps (asStream p) 0
       where
-        go 0 _ _ = return []
+        go 0 _ _ = pure []
         go n s t = do
           (x, s') <- runStream s t
           events <- go (n-1) s' t
-          return (Event t 0 (Map.singleton "value" x) : events)
+          pure (Event t 0 (Map.singleton "value" x) : events)
 
 -- | Schedules and plays events.
 scheduleEvents :: [Event] -> IO ()
@@ -181,13 +181,13 @@ scheduleEvents events = do
       threadDelay $ floor $ delay * 1000000  -- Convert to microseconds
       print event  -- In a real implementation, this would trigger sound playback
 
--- | Main function demonstrating the use of the pattern system.
+
 main :: IO ()
 main = do
-    -- Define patterns for duration, frequency, and amplitude
-    let durPattern = pwhite 0.00001 0.5  -- Random durations between 0.00001 and 0.5 seconds
-        freqPattern = pgeom 100 1.05 12  -- Geometric series of frequencies, starting at 100 Hz
-        ampPattern = pwhite 0.1 10.8     -- Random amplitudes between 0.1 and 10.8
+
+    let durPattern = pwhite 0.00001 0.5  
+        freqPattern = pgeom 100 1.05 12 
+        ampPattern = pwhite 0.1 10.8    
 
         -- Combine the patterns into a single event pattern
         myPattern = pbind $ fromList
