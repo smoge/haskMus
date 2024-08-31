10a11ef53627359d1535b68f31f45771c4ebc3b2
Bernardo Barros <bernardobarros@gmail.com>
2024-08-31 08:21:07 -0400

    Update Time.Pattern module

--- src/Time/Pattern.hs
+++ src/Time/Pattern.hs
@@ -16,6 +16,7 @@ import Data.Maybe (fromMaybe)
 
 import Debug.Trace (trace)
 import Control.DeepSeq (deepseq)
+import System.Clock (Clock(Monotonic), getTime, toNanoSecs)
 
 type Time = Double
 type Duration = Double
@@ -58,12 +59,12 @@ pscale scale = Pattern $ \t _ ->
 -- Bind multiple patterns into an event.
 pbind :: Map String (Pattern Double) -> Pattern Event
 pbind paramPatterns = Pattern $ \t gen ->
-  let (paramValues, gen') = foldr
-        (\(k, v) (acc, g) ->
+  let (paramValues, gen') = Map.foldrWithKey
+        (\k v (acc, g) ->
           let (val, g') = runPattern v t g
           in ((k, val):acc, g'))
         ([], gen)
-        (Map.toList paramPatterns)
+        paramPatterns
       params = Map.fromList paramValues
       dur = Map.findWithDefault 0 "dur" params
       event = Event t dur params
@@ -72,19 +73,29 @@ pbind paramPatterns = Pattern $ \t gen ->
 midiToFreq :: Double -> Double
 midiToFreq n = 440 * (2 ** ((n - 69) / 12))
 
-copyList :: [(a, b)] -> [(a, b)]
-copyList = fmap (\(x, y) -> (x, y))
+normalize :: [Double] -> [Double]
+normalize xs = fmap (/ totalSum) xs
+  where
+    totalSum = sum xs
+
+
+normalizeSum :: [(a, Double)] -> [(a, Double)]
+normalizeSum [] = error "normalizeSum: empty list of items" 
+normalizeSum xs = fmap (\(x, w) -> (x, abs w / totalSum)) xs
+  where
+    totalSum = sum $ fmap (abs . snd) xs
+
 
 pchoose :: [(Double, Double)] -> Pattern Double
 pchoose weightedItemsOrig
     | null weightedItemsOrig = error "pchoose: empty list of items"
     | otherwise =
-        let weightedItems = copyList weightedItemsOrig
+        let weightedItems = weightedItemsOrig
         in weightedItems `deepseq` Pattern $ \_ gen ->
             let totalWeight = sum $ fmap snd weightedItems
                 cumWeights = case scanl' (+) 0 $ fmap snd weightedItems of
                   _ : xs -> xs
-                  [] -> error "pchoose: empty list of items"
+                  [] -> error "pchoose: empty list after scanl'"
                 (r, gen') = randomR (0, totalWeight) gen
             in trace ("selected item with r: " <> show r) (selectItem r weightedItems cumWeights, gen')
   where
@@ -100,15 +111,16 @@ ensureNonZeroWeights items =
     let minWeight = 1e-6  -- A small, non-zero value
     in fmap (\(x, w) -> (x, max w minWeight)) items
 
+
 scheduleEvents :: [Event] -> IO ()
 scheduleEvents events = do
-  startTime <- getPOSIXTime
-  mapM_ (playEvent (realToFrac startTime)) events
+  startTime <- getTime Monotonic
+  mapM_ (playEvent startTime) events
   where
     playEvent startTime event = do
-      currentTime <- getPOSIXTime
-      let eventTime = startTime + eTime event
-          delay = max 0 $ eventTime - realToFrac currentTime
+      currentTime <- getTime Monotonic
+      let eventTime = fromIntegral (toNanoSecs startTime) / 1e9 + eTime event
+          delay = max 0 $ eventTime - fromIntegral (toNanoSecs currentTime) / 1e9
       threadDelay $ floor $ delay * 1000000
       print event
 
@@ -122,15 +134,26 @@ runPatternIO n p = do
         go nextT (i - 1) gen' (event : acc)
   go 0 n initialGen []
 
+-- Transform a uniform random variable to an exponentially distributed one.
+uniformToExponential :: Double -> Double -> Double
+uniformToExponential λ u
+  | u > 0 && u < 1 = (- log (1 - u)) / λ
+  | otherwise = error "Uniform random variable must be in the range (0, 1)."
+
+pexponential :: Double -> Pattern Double
+pexponential lambda = getRandom $ \gen ->
+  let (u, gen') = randomR (0.0, 1.0) gen
+  in (uniformToExponential lambda u, gen')
+
+
 main :: IO ()
 main = do
-    let durPattern = pwhite 0.011 0.3
+    let durPattern = fmap (* 1.5) (pexponential 9)
         freqPattern = fmap midiToFreq (pbrown 60 72 1)
         ampPattern = pwhite 0.1 0.8
-        rawScalePattern = [(60, 0.2), (62, 0.2), (64, 0.2), (65, 0.2), (67, 0.2)]
+        rawScalePattern = normalizeSum [(60, 0.2), (62, -0.2), (64, 0.5), (65, 0.2), (67, 0.2)]
         scalePattern = pchoose $ ensureNonZeroWeights rawScalePattern
 
-    trace ("rawScalePattern: " <> show rawScalePattern) $ pure ()
 
     let myPattern = pbind $ Map.fromList
             [ ("dur", durPattern)
@@ -141,3 +164,4 @@ main = do
 
     events <- runPatternIO 50 myPattern
     scheduleEvents events
+    trace ("rawScalePattern: " <> show rawScalePattern) $ pure ()
