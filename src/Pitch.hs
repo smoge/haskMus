8adf42b0f74ba29ca45433fcc7cc4bb9b3414152
Bernardo Barros <bernardobarros@gmail.com>
2024-09-16 11:45:09 -0400

    update

--- src/Pitch.hs
+++ src/Pitch.hs
@@ -34,7 +34,6 @@ instance HasNoteName PitchClass where
   getNoteName = PC.noteName
   setNoteName newName pc = pc { PC.noteName = newName }
 
--- Type class for types that have an Accidental
 class HasAccidental a where
   getAccidental :: a -> Accidental
   setAccidental :: Accidental -> a -> a
@@ -47,24 +46,19 @@ instance HasAccidental PitchClass where
   getAccidental = PC.accidental
   setAccidental newAcc pc = pc { PC.accidental = newAcc }
 
--- Define a type family to resolve types for `Updatable`
 type family IsList a where
   IsList [x] = True
   IsList x   = False
 
--- Updatable class with type family dispatch
 class Updatable b a where
   (=:) :: b -> a -> a
 
--- For updating NoteName
 instance (HasNoteName a, IsList a ~ False) => Updatable NoteName a where
   (=:) = setNoteName
 
--- For updating Accidental
 instance (HasAccidental a, IsList a ~ False) => Updatable Accidental a where
   (=:) = setAccidental
 
--- Instance for updating elements within a list
 instance (Updatable b a, IsList a ~ True) => Updatable b [a] where
   (=:) x = fmap (x =:)
 
@@ -86,3 +80,12 @@ decrementOctave pitch = pitch { octave = Octave (pitch.octave.unOctave - 1) }
 
 modifyListWithComprehension :: [Pitch] -> [Pitch]
 modifyListWithComprehension pitches = [if p.noteName == D then addOctave p 1 else p | p <- pitches]
+
+modifyPitch :: Pitch -> Pitch
+modifyPitch p@(Pitch C _ _) = addOctave p 1  -- Only modify if noteName is C
+modifyPitch p@(Pitch _ Flat _) = addOctave p (-1) -- Decrease octave if accidental is Flat
+modifyPitch p = p  -- Leave other pitches unchanged
+
+modifyOctavesWithGuards :: [Pitch] -> [Pitch]
+modifyOctavesWithGuards = fmap modifyPitch
+
